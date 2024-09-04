b587698159cbac489f889a22c9f8e1da59320739
Alex <34627441+alex-yatseyko@users.noreply.github.com>
2024-09-04 13:26:50 -0400

    web app initated

---
+++ projects/Software_developer_salary_prediction/README.md
@@ -0,0 +1,16 @@
+# Software Developer Salary Prediction Web App
+
+This is a web application built using Streamlit that predicts software developer salaries based on various input parameters. It leverages machine learning to provide salary estimates, helping individuals and organizations make informed decisions.
+
+## Features
+
+- **Input Parameters**: Enter details such as years of experience, education level and location
+- **Salary Prediction**: Get an estimated salary based on the input parameters.
+- **User-Friendly Interface**: A simple and interactive web interface for easy use.
+
+## Prerequisites
+
+Before you start, ensure you have met the following requirements:
+- Python 3.7 or higher
+- pip (Python package installer)
+
---
+++ projects/Software_developer_salary_prediction/app/predict_page.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+def load_model():
+    with open('../model.pkl', 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+data = load_model()
+
+regressor = data['model']
+country_encoder = data['country_encoder']
+education_encoder = data['education_encoder']
+
+def show_predict_page():
+    st.title("Software Developer Salary Prediction")
+    st.write("""#### We need some information to predict the salary""")
+
+    countries = (
+        "Other",
+        "United States of America",
+        "Germany",
+        "United Kingdom of Great Britain and Northern Ireland",
+        "Ukraine",
+        "India",
+        "France",
+        "Canada",
+        "Brazil",
+        "Spain",
+        "Italy",
+        "Netherlands",
+        "Australia"
+    )
+
+    educations = (
+        'Post grad', 'Master degree', 'Less than a Bachelor', 'Bachelor degree'
+    )
+
+    country = st.selectbox("Country", countries)
+    education = st.selectbox("Education", educations)
+
+    experience = st.slider("Years of experience", 0, 50, 3)
+
+    ok = st.button('Calculate Salary')
+    if ok:
+        X = np.array([[country, education, experience]])
+        X[:,0] = country_encoder.transform(X[:,0])
+        X[:,1] = education_encoder.transform(X[:,1]) 
+        X = X.astype(float)
+
+        salary = regressor.predict(X)
+        st.subheader(f"The estimated salary is ${salary}")
